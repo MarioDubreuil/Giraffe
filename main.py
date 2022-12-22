3257c76e218caa15c9cae78468441ded84fd9253
Mario Dubreuil <mario_dubreuil@hotmail.com>
2022-12-21 20:53:14 -0500

    variables

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-print("   /|")
-print("  / |")
-print(" /  |")
-print("/___|")
+character_name = "John"
+character_age = 35
+is_male = True
+
+print("There once was a man named " + character_name + ", ")
+print("he was " + str(character_age) + " years old. ")
+print("He really liked the name " + character_name + ", ")
+print("but didn't like being " + str(character_age) + ".")
+print("is he male? " + str(is_male))
